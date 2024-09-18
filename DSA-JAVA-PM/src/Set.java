27bcd285fb781545f0e2c99fd219e5f5c3b87baf
Matongo prince <davidkitoko13@gmail.com>
2024-09-18 17:31:15 +0300

    modification naming

--- DSA-JAVA-PM/src/Set.java
+++ DSA-JAVA-PM/src/Set.java
@@ -6,27 +6,27 @@ public class Set {
     public static void main(String[] args){
 
 
-        HashSet<String> week = new HashSet<>();
-        week.add("Monday");
-        week.add("Tuesday");
-        week.add("Wednesday");
-        week.add("Thursday");
-        week.add("Friday");
-        week.add("Saturday");
-        week.add("Sunday");
-        System.out.println("\t Week Hash Set: "+week);
+        HashSet<String> days = new HashSet<>();
+        days.add("Monday");
+        days.add("Tuesday");
+        days.add("Wednesday");
+        days.add("Thursday");
+        days.add("Friday");
+        days.add("Saturday");
+        days.add("Sunday");
+        System.out.println("\t Week Hash Set: "+days);
 
-        LinkedHashSet<String> week2 = new LinkedHashSet<>();
-        week2.add("Monday");
-        week2.add("Tuesday");
-        week2.add("Wednesday");
-        week2.add("Thursday");
-        week2.add("Friday");
-        week2.add("Saturday");
-        week2.add("Sunday");
-        System.out.println("\t Week linked Hash Set: "+week2);
+        LinkedHashSet<String> orderedDays = new LinkedHashSet<>();
+        orderedDays.add("Monday");
+        orderedDays.add("Tuesday");
+        orderedDays.add("Wednesday");
+        orderedDays.add("Thursday");
+        orderedDays.add("Friday");
+        orderedDays.add("Saturday");
+        orderedDays.add("Sunday");
+        System.out.println("\t Week linked Hash Set: "+orderedDays);
 
-        boolean equal = week.equals(week2);
+        boolean equal = orderedDays.equals(days);
         if(equal){
             System.out.println("\t is week is equal to week2 ");
         }else{
