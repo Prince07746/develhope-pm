73b197a86a8bb4938c9bc6dd0bec6c5466e35511
Matongo prince <davidkitoko13@gmail.com>
2024-10-20 03:45:55 +0300

    pushing data about interface

---
+++ DSA-JAVA-PM/src/DataStructure/MergeSortedList.java
@@ -0,0 +1,22 @@
+package DataStructure;
+
+import java.util.List;
+
+public class MergeSortedList {
+    public static void main(String[] args){
+
+
+
+    }
+
+}
+
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
